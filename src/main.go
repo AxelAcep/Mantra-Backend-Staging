8f7de1675d028d31715fc0138f451a10c8b534f2
Axel Sebayang <axelsebayang456@gmail.com>
2026-03-24 19:47:31 +0700

    Whatever Is it

--- src/main.go
+++ src/main.go
@@ -22,7 +22,7 @@ func main() {
 	config.ConnectDB()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:5173"},
+		AllowOrigins: []string{"http://localhost:5173", "http://192.168.1.13:5173"},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodPatch},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAuthorization},
 	}))
